5ee1d80dc167d4d2612782e4eb288387fc37a04d
Triston Yoder <triston@7co.dev>
2025-10-23 16:07:47 -0400

    Fix boot loader: remove conflicting GRUB configuration
    - Remove GRUB settings that conflict with systemd-boot
    - Keep only systemd-boot configuration for UEFI systems
    - Fixes issue #2: system was booting from old EFI partition

--- modules/hardware/boot.nix
+++ modules/hardware/boot.nix
@@ -6,29 +6,16 @@ let
 in
 {
   options.modules.hardware.boot = {
-    enable = mkEnableOption "Bootloader configuration (systemd-boot with GRUB theming)";
+    enable = mkEnableOption "Bootloader configuration (systemd-boot)";
     
     configurationLimit = mkOption {
       type = types.int;
       default = 50;
       description = "Number of boot generations to keep";
     };
-    
-    enableGrubTheme = mkOption {
-      type = types.bool;
-      default = true;
-      description = "Enable GRUB theme (Breeze)";
-    };
-    
-    backgroundColor = mkOption {
-      type = types.str;
-      default = "#8275b4";
-      description = "GRUB background color";
-    };
   };
 
   config = mkIf cfg.enable {
-    # Bootloader configuration
     boot.loader = {
       systemd-boot = {
         enable = true;
@@ -36,14 +23,6 @@ in
       };
       
       efi.canTouchEfiVariables = true;
-      
-      grub = {
-        efiSupport = true;
-        splashMode = "normal";
-        backgroundColor = cfg.backgroundColor;
-        theme = mkIf cfg.enableGrubTheme "${pkgs.libsForQt5.breeze-grub}/grub/themes/breeze";
-        extraEntriesBeforeNixOS = true;
-      };
     };
   };
 }
