fec4d7a8074498728ce6a0ddf4075eabeac3d811
Triston Yoder <triston@7co.dev>
2026-03-28 01:18:54 -0400

    fix: set tidarr to jellyfin:media and chown Music to jellyfin:media

--- docker/media/media-aq.nix
+++ docker/media/media-aq.nix
@@ -475,7 +475,7 @@
     ];
     environment = {
       "PGID" = "970";   # media group
-      "PUID" = "1000";
+      "PUID" = "994";   # jellyfin user
       "UMASK" = "0002"; # group-writable so media group members can modify
       "TZ" = config.time.timeZone;
       "JELLYFIN_URL" = "http://localhost:${toString config.modules.services.media.jellyfin.port}";
--- modules/services/media/beets.nix
+++ modules/services/media/beets.nix
@@ -57,7 +57,7 @@ in
 
     user = mkOption {
       type = types.str;
-      default = "tristonyoder";
+      default = "jellyfin";
       description = "User to run beets as";
     };
 
