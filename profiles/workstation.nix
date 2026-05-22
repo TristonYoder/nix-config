b0ff92159a13a23e167dad1b5901c798178763c8
Triston Yoder <triston@7co.dev>
2026-05-21 21:15:10 -0400

    fix: use kdePackages.plasma-browser-integration for Plasma 6

--- profiles/workstation.nix
+++ profiles/workstation.nix
@@ -24,7 +24,7 @@
   environment.systemPackages = with pkgs; [
     # Web browsers
     firefox
-    plasma-browser-integration
+    kdePackages.plasma-browser-integration
 
     # Media players
     vlc
